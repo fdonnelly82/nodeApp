d8eaec79b00b3f2b84a4f1ff987fdc99bd34cc83
1302264 <fdonnelly82@gmail.com>
2015-11-23 14:21:33 +0000

    Update on server.js file, now has html

--- server.js
+++ server.js
@@ -2,10 +2,23 @@
  * Created by 1302264 on 23/11/2015.
  */
 
-var http = require('http')
+var http = require('http');
 var port = process.env.PORT || 1337;
 
 http.createServer(function(request, responce) {
-    responce.writeHead(200, { 'Content-Type' : 'text/plain' });
-    responce.end('Hello World\n');
-}).listen(port);
+    responce.writeHead(200, { 'Content-Type' : 'text/html' });
+    responce.write("<!DOCTYPE 'html'>");
+    responce.write("<html>");
+    responce.write("<head>");
+    responce.write("<title>Hello World Page</title>");
+    responce.write("</head>");
+    responce.write("<body>");
+    responce.write("<h1>");
+    responce.write("Hello World!");
+    responce.write("</h1>");
+    responce.write("</body>");
+    responce.write("</html>");
+    responce.end();
+});
+server.listen(port);
+console.log("Server is listening");
