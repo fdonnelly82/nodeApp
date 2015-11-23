bf1bee3178bc688335eab529c9bee34fc3164d10
1302264 <fdonnelly82@gmail.com>
2015-11-23 15:09:26 +0000

    package.json file links server file with database, now connects to database with authentication

--- server.js
+++ server.js
@@ -2,23 +2,4 @@
  * Created by 1302264 on 23/11/2015.
  */
 
-var http = require('http');
-var port = process.env.PORT || 1337;
-
-var server = http.createServer(function(request, responce) {
-    responce.writeHead(200, { 'Content-Type' : 'text/html' });
-    responce.write("<!DOCTYPE 'html'>");
-    responce.write("<html>");
-    responce.write("<head>");
-    responce.write("<title>Hello World Page</title>");
-    responce.write("</head>");
-    responce.write("<body>");
-    responce.write("<h1>");
-    responce.write("Hello World!");
-    responce.write("</h1>");
-    responce.write("</body>");
-    responce.write("</html>");
-    responce.end();
-});
-server.listen(port);
-console.log("Server is listening");
+require('./connect');
