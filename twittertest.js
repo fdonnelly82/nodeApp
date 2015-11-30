db53d939089e0c4132aa07fef7031bf08e4f284a
1302264 <fdonnelly82@gmail.com>
2015-11-30 16:11:42 +0000

    adds link to twitter via node, displayed in html, looks better in php page (only shows name and tweet text)

--- twittertest.js
+++ twittertest.js
@@ -4,6 +4,7 @@
 var Twitter = require('twitter');
 var http = require('http')
 var port = process.env.PORT || 1337;
+var json = [];
 
 var client = new Twitter({
     consumer_key: 'NVtfmoGndxwEtOYHvtwbCaXHT',
@@ -12,9 +13,14 @@ var client = new Twitter({
     access_token_secret: 'MPThhPwIK4TBBapNQjms8WPKJbdSkbjwLLBUF6rNuYkH7'
 });
 
+for (var i = 0; i<tweets.statuses.length ; i++)
+{
+    json.push({name: tweets.statuses[i].user.name, text: tweets.statuses[i].text});
+}
+
 http.createServer(function(request, responce) {
     responce.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*' });
     client.get('search/tweets', {q: 'lolcat'}, function(error, tweets) {
-        responce.end(JSON.stringify(tweets));
+        responce.end(JSON.stringify(json));
     });
 }).listen(port);
